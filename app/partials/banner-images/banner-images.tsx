9edb6ffc92db93e85c03c7b27b1ba48efec6dcbe
Quân (Máy cty) <pthyst2@gmail.com>
2022-08-30 17:25:17 +0700

    dasdasd

---
+++ app/partials/banner-images/banner-images.tsx
@@ -0,0 +1,11 @@
+
+export default function BannerImages(){
+    
+    return (
+        <>
+            <div className="absolute top-0 bottom-0 min-w-[100%] h-[100vh]">
+        
+            </div>
+        </>
+    )
+}
